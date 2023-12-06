00e00174b0d96dafb501bef48c98ed0d1ebe50d2
Arzhancev Michael TV-11 <104345619+Akella11@users.noreply.github.com>
2023-12-06 03:39:38 +0200

    Create silabus_data2.php

---
+++ silabus_data2.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class silabus_data2 extends Model
+{
+    public $table = 'silabus_data2';
+    public $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = false;
+}
